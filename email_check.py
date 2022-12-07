2290e9fe90f7527fb3fa9b065557695c5b9bd02c
Rudraunsh12222395 <118504886+Rudraunsh12222395@users.noreply.github.com>
2022-12-07 15:39:08 +0530

    Email checker

---
+++ email_check.py
@@ -0,0 +1,21 @@
+import re
+def emailcheck(email):
+    if(re.match(r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[a-zA-Z]{2,}",email)):
+        return True
+    else:
+        return False
+def passwordcheck(password):
+    if (re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&*])[a-zA-Z0-9@#$%^&*]{6,20}",password)):
+        return True
+    else:
+        return False
+email = input()
+password = input()
+if (emailcheck(email)==True):
+    print("valid email")
+else:
+    print("invalid email")
+if (passwordcheck(password)==True):
+    print("valid password")
+else:
+    print("invalid password")                                   
